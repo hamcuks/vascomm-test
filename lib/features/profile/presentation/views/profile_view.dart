1aa17596db17abea4a9c9cd63398ec8ba8ec00cc
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-04-27 15:56:54 +0700

    feat(app): create custom tab bar

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -4,12 +4,26 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:vascomm_test/shared/presentation/widgets/app_button.dart';
 import 'package:vascomm_test/shared/presentation/widgets/app_drawer.dart';
 import 'package:vascomm_test/shared/presentation/widgets/app_input_form.dart';
+import 'package:vascomm_test/shared/presentation/widgets/custom_tab_bar.dart';
 import 'package:vascomm_test/shared/presentation/widgets/notification_banner.dart';
 
-class ProfileView extends StatelessWidget {
-  ProfileView({super.key});
+class ProfileView extends StatefulWidget {
+  const ProfileView({super.key});
 
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView>
+    with TickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _key = GlobalKey();
+  late final TabController _controller;
+
+  @override
+  void initState() {
+    _controller = TabController(length: 2, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,153 +52,174 @@ class ProfileView extends StatelessWidget {
           ],
         ),
         endDrawer: const AppDrawer(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 16),
-                        color: const Color(0xFFBEBEBE).withOpacity(0.16),
-                        blurRadius: 24,
-                      )
-                    ],
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: CustomTabBar(
+                controller: _controller,
+                tabs: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Profil Saya'),
                   ),
-                  child: Column(
-                    children: [
-                      const _ProfileCard(),
-                      const SizedBox(height: 16),
-                      Text(
-                        'Pilih data yang ingin ditampilkan',
-                        style: GoogleFonts.poppins(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: const Color(0xFF002060),
-                        ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Pengaturan'),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: const Offset(0, 16),
+                            color: const Color(0xFFBEBEBE).withOpacity(0.16),
+                            blurRadius: 24,
+                          )
+                        ],
                       ),
-                      const SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      child: Column(
                         children: [
+                          const _ProfileCard(),
+                          const SizedBox(height: 16),
+                          Text(
+                            'Pilih data yang ingin ditampilkan',
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: const Color(0xFF002060),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 48,
+                                    height: 48,
+                                    decoration: const BoxDecoration(
+                                      color: Color(0xFF00D9D5),
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: const Icon(
+                                      Icons.assignment_ind,
+                                      color: Color(0xFF002060),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Data Diri',
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                          color: const Color(0xFF333333),
+                                        ),
+                                      ),
+                                      Text(
+                                        'Data diri anda sesuai KTP',
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.w600,
+                                          color: const Color(0xFFBDBDBD),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              const SizedBox(width: 16),
                               Container(
                                 width: 48,
                                 height: 48,
                                 decoration: const BoxDecoration(
-                                  color: Color(0xFF00D9D5),
+                                  color: Color(0xFFEBEDF7),
                                   shape: BoxShape.circle,
                                 ),
                                 child: const Icon(
-                                  Icons.assignment_ind,
-                                  color: Color(0xFF002060),
+                                  Icons.location_on,
+                                  color: Color(0xFF6A6A6A),
                                 ),
                               ),
-                              const SizedBox(width: 8),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Data Diri',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w600,
-                                      color: const Color(0xFF333333),
-                                    ),
-                                  ),
-                                  Text(
-                                    'Data diri anda sesuai KTP',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w600,
-                                      color: const Color(0xFFBDBDBD),
-                                    ),
-                                  ),
-                                ],
-                              )
                             ],
                           ),
-                          const SizedBox(width: 16),
-                          Container(
-                            width: 48,
-                            height: 48,
-                            decoration: const BoxDecoration(
-                              color: Color(0xFFEBEDF7),
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Icon(
-                              Icons.location_on,
-                              color: Color(0xFF6A6A6A),
+                          const SizedBox(height: 16),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Form(
+                              child: Column(
+                                children: [
+                                  const AppInputForm(
+                                    title: 'Nama Depan',
+                                  ),
+                                  const SizedBox(height: 30),
+                                  const AppInputForm(
+                                    title: 'Nama Belakang',
+                                  ),
+                                  const SizedBox(height: 30),
+                                  const AppInputForm(
+                                    title: 'Email',
+                                  ),
+                                  const SizedBox(height: 30),
+                                  const AppInputForm(
+                                    title: 'No Telepon',
+                                  ),
+                                  const SizedBox(height: 30),
+                                  const AppInputForm(
+                                    title: 'No. KTP',
+                                  ),
+                                  const SizedBox(height: 34),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.info,
+                                        color: Color(0xFF002060),
+                                      ),
+                                      const SizedBox(width: 16),
+                                      Flexible(
+                                        child: Text(
+                                          'Pastikan profile anda terisi dengan benar, data pribadi anda terjamin keamanannya',
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 12,
+                                            color: const Color(0xFF6A6A6A),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 34),
+                                  AppButton(
+                                    title: 'Simpan Profil',
+                                    icon: const Icon(Icons.save),
+                                    onPressed: () {},
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 16),
-                      Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Form(
-                          child: Column(
-                            children: [
-                              const AppInputForm(
-                                title: 'Nama Depan',
-                              ),
-                              const SizedBox(height: 30),
-                              const AppInputForm(
-                                title: 'Nama Belakang',
-                              ),
-                              const SizedBox(height: 30),
-                              const AppInputForm(
-                                title: 'Email',
-                              ),
-                              const SizedBox(height: 30),
-                              const AppInputForm(
-                                title: 'No Telepon',
-                              ),
-                              const SizedBox(height: 30),
-                              const AppInputForm(
-                                title: 'No. KTP',
-                              ),
-                              const SizedBox(height: 34),
-                              Row(
-                                children: [
-                                  const Icon(
-                                    Icons.info,
-                                    color: Color(0xFF002060),
-                                  ),
-                                  const SizedBox(width: 16),
-                                  Flexible(
-                                    child: Text(
-                                      'Pastikan profile anda terisi dengan benar, data pribadi anda terjamin keamanannya',
-                                      style: GoogleFonts.poppins(
-                                        fontSize: 12,
-                                        color: const Color(0xFF6A6A6A),
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              const SizedBox(height: 34),
-                              AppButton(
-                                title: 'Simpan Profil',
-                                icon: const Icon(Icons.save),
-                                onPressed: () {},
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  const NotificationBanner(),
+                ],
               ),
-              const NotificationBanner(),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/shared/presentation/widgets/custom_tab_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTabBar extends StatelessWidget {
+  const CustomTabBar({
+    super.key,
+    required TabController controller,
+    required this.tabs,
+  }) : _controller = controller;
+
+  final TabController _controller;
+  final List<Widget> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(200),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 16),
+            color: const Color(0xFFBEBEBE).withOpacity(0.16),
+            blurRadius: 24,
+          )
+        ],
+      ),
+      child: TabBar(
+        indicatorSize: TabBarIndicatorSize.tab,
+        indicator: BoxDecoration(
+          color: const Color(0xFF00D9D5),
+          borderRadius: BorderRadius.circular(200),
+        ),
+        controller: _controller,
+        labelStyle: GoogleFonts.poppins(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: const Color(0xFF002060),
+        ),
+        unselectedLabelStyle: GoogleFonts.poppins(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: const Color(0xFF002060),
+        ),
+        dividerColor: Colors.transparent,
+        tabs: tabs,
+      ),
+    );
+  }
+}
