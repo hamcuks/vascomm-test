eb714385fa435d09a86892689a38d4f84c671278
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-04-27 17:07:53 +0700

    feat(styles): add color variable

---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color kTitle = Color(0xFF002060);
+const Color kSecondary = Color(0xFF00D9D5);
+const Color kGrey = Color(0xFFBEBEBE);
+const Color kDarkGrey = Color(0xFF6A6A6A);
+const Color kOrange = Color(0xFFFF7200);
